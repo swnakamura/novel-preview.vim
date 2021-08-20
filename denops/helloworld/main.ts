c1ba33a05037fe2b793fa69fef808db4e93a41da
woodyZootopia <piequalsabout314159@gmail.com>
2021-08-20 09:38:38 +0900

    バッファの中身をwebsocketで送信

--- denops/helloworld/main.ts
+++ denops/helloworld/main.ts
@@ -8,32 +8,10 @@ import { fromFileUrl } from "https://deno.land/std@0.105.0/path/mod.ts";
 import { create_index_html } from "./server.ts";
 import type { WebSocket } from "https://deno.land/std@0.105.0/ws/mod.ts";
 
-const app = createApp();
-
-// index.htmlにアクセスされたときだけはbodyHTMLを返す
-let index_url = new URL("./index.html", import.meta.url);
-let bodyHTML = Deno.readTextFileSync(fromFileUrl(index_url));
-app.get("/", async (req) => {
-  await req.respond({
-    status: 200,
-    headers: new Headers({
-      "content-type": "text/html",
-    }),
-    body: bodyHTML,
-  });
-});
-
-// それ以外のときはstaticの中身をそのまま返す
-let misc_url = new URL("./static", import.meta.url);
-let misc_root_directory = fromFileUrl(misc_url);
-app.use(serveStatic(misc_root_directory));
-
-app.listen({ port: 8899 });
-
 export async function main(denops: Denops): Promise<void> {
   // denopsコマンドを定義
   await denops.cmd(
-    `command! Up call denops#request('${denops.name}', 'updateServer', [])`,
+    `command! Up call denops#request('${denops.name}', 'startServer', [])`,
   );
 
   // dispatcherを定義
@@ -42,36 +20,56 @@ export async function main(denops: Denops): Promise<void> {
       ensureString(text);
       return await Promise.resolve(text);
     },
-    async updateServer(): Promise<unknown> {
-      let bufferContentList = (await denops.eval("getline(1, '$')")) as [
-        string,
-      ];
-      var bufferContent = bufferContentList.map((x) =>
-        '<p class="honbun">' + pixivFormatter(x) + "</p>"
-      ).join(
-        "",
-      );
-      bodyHTML = create_index_html(bufferContent);
+    async startServer(): Promise<unknown> {
+      // サーバを立てる
+      const app = createApp();
+
+      // index.htmlにアクセスされたときだけはbodyHTMLを返す
+      const index_url = new URL("./index.html", import.meta.url);
+      const bodyHTML = Deno.readTextFileSync(fromFileUrl(index_url));
+      app.get("/", async (req) => {
+        await req.respond({
+          status: 200,
+          headers: new Headers({
+            "content-type": "text/html",
+          }),
+          body: bodyHTML,
+        });
+      });
+
+      // それ以外のときはstaticの中身をそのまま返す
+      let misc_url = new URL("./static", import.meta.url);
+      let misc_root_directory = fromFileUrl(misc_url);
+      app.use(serveStatic(misc_root_directory));
+      app.listen({ port: 8899 });
+
+      // websocketサーバを立てる
+      const ws_app = createApp();
+      ws_app.ws("/ws", handleHandShake);
+      ws_app.listen({ port: 8900 });
+
+      function handleHandShake(sock: WebSocket) {
+        async function handleMessage(sock: WebSocket) {
+          for await (const msg of sock) {
+            if (typeof msg === "string") {
+              let bufferContentList =
+                (await denops.eval("getline(1, '$')")) as [
+                  string,
+                ];
+              var bufferContent = bufferContentList.map((x) =>
+                '<p class="honbun">' + pixivFormatter(x) + "</p>"
+              ).join(
+                "",
+              );
+              sock.send(bufferContent);
+            }
+          }
+        }
+        handleMessage(sock);
+      }
       return await Promise.resolve();
     },
   };
-
-  // websocketサーバを立てる
-  const ws_app = createApp();
-  ws_app.ws("/ws", handleHandShake);
-  ws_app.listen({ port: 8900 });
-
-  function handleHandShake(sock: WebSocket) {
-    async function handleMessage(sock: WebSocket) {
-      for await (const msg of sock) {
-        if (typeof msg === "string") {
-          sock.send(msg);
-          // await denops.cmd(`echo WOW`);
-        }
-      }
-    }
-    handleMessage(sock);
-  }
 }
 
 function pixivFormatter(x: string) {
--- denops/helloworld/static/js/websocket.js
+++ denops/helloworld/static/js/websocket.js
@@ -1,10 +1,12 @@
-var conn = new WebSocket("ws://localhost:8900/ws")
-conn.onopen = function() {
-    console.log("connection opened!");
-}
-conn.onmessage = function(event) {
-    document.getElementById("eventType").innerHTML = "メッセージ受信";
-    document.getElementById("dispMsg").innerHTML = event.data;
-    console.log(event.data);
-}
-conn.send("HI!");
+var conn = new WebSocket("ws://localhost:8900/ws");
+conn.onopen = function () {
+  console.log("connection opened!");
+};
+conn.onmessage = function (event) {
+  document.getElementById("preview").innerHTML = event.data;
+  console.log(event.data);
+};
+
+window.setInterval(() => {
+  conn.send("HI");
+}, 3000);
